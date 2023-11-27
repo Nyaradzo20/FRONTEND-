b73eeb768cd9408b70f92c01836e212c6bc80c0f
Nyaradzo20 <nchinyati20@gmail.com>
2023-11-27 13:29:49 +0200

    react app

---
+++ react/React-app/src/Message.tsx
@@ -0,0 +1,11 @@
+
+//PascalCasing- Capitalize the first letter of each word
+function Message() {
+    //this syntax is jssx which is short for javascript xml
+
+const name = "Mosh"
+    return <h1>Hello {name}</h1>;
+}
+
+
+export default Message;
